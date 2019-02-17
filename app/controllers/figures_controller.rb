4f792aeaab759e382ba3a8c91fbc798d3a170659
Nicole D <nnardella@gmail.com>
2019-02-17 01:51:07 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -12,16 +12,22 @@ class FiguresController < ApplicationController
   end
 
   post "/figures" do
+    binding.pry
     @figure = Figure.create(params[:figure])
-    if !params["title"]["name"].empty?
-         @figure.titles << Title.create(name: params["title"]["name"])
+    if !params[:title][:name].empty?
+         @figure.titles << Title.create(name: params[:title][:name])
+      else
+        @titles = Title.find_by(params[:figure][:title_ids])
+        @figure.titles << @titles
         end
 
-    if !params["figure"]["landmark"].empty?
-    @figure.landmarks << Landmark.create(name: params["landmark"]["name"], year_completed: params["landmark"]["year_completed"])
+    if !params[:landmark].empty?
+    @figure.landmarks << Landmark.create(params[:landmark])
     end
+
     redirect "figures/#{@figure.id}"
   end
+
 
   get '/figures/:id/edit' do
 
